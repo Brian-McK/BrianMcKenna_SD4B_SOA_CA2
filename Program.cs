774f771b64acf0a2aa47ad1508dd155eac9cd16b
Brian McKenna <D00197352@student.dkit.ie>
2022-11-29 21:13:43 +0000

    Added repositories to program cs file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 using BrianMcKenna_SD4B_SOA_CA2.Models;
+using BrianMcKenna_SD4B_SOA_CA2.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<BoxingClubContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BoxingClubDatabase")));
+builder.Services.AddScoped<BoxerRepository>();
+builder.Services.AddScoped<TrainerRepository>();
+builder.Services.AddScoped<WeightLogRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
